47c9f0a192fa4a4cbb39cae9348be73a3b984851
Yom11 <sf_b0i66@yahoo.com>
2017-03-11 18:26:45 +0800

    Add fibonacci problem and bootstrap codes in my folder

--- js/script.js
+++ js/script.js
@@ -2,29 +2,25 @@ function probOne(){
 	var divBy1 = parseInt(document.getElementById('div1').value);
 	var divBy2 = parseInt(document.getElementById('div2').value);
 	var range = parseInt(document.getElementById('range').value);
+	var regex = /^[0-9\.]+$/;
 	var sum = 0;
+
 	for (var i = 0; i < range; i++){
 		if(i % divBy1 === 0 || i % divBy2 === 0){
 			sum += i;
 		}
 	} 
-	// sum = divBy + range;
+
+    if(regex.test(divBy1) && regex.test(divBy2) && regex.test(range)){
+        // return true;
+    }
+    else {
+        alert("Please enter a number only. Strings is not allowed!");
+        return false;
+    }
 
 	document.getElementById('answer').value = sum;
 }
-// function q1Calc(){
-// 		var no = parseInt(document.getElementById('q1').value);
-// 		var sum = 0;
-// 		for (var i = 1; i< no; i++)
-// 		{
-// 			if ( i % 3 === 0 || i%5 === 0)
-// 			{
-// 				sum = sum + i;
-// 			}
-// 		}
-// 		document.getElementById('a1').value = sum;
-// }
-
 
 
 
